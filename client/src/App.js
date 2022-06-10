66cad5a39c3b188e6ab25416f44455800bdd228c
Alok Yadav <alokyadav9821238248@gmail.com>
2022-06-10 14:25:57 +0530

    just before redux setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,32 @@
 import React from 'react'
-
+import {Container , AppBar , Typography, Grow , Grid} from '@material-ui/core';
+import Memories from './images/memories.png';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form'
+import useStyles from './styles';
 const App = () => {
+  const classes = useStyles();
   return (
-    <>
-    <h1>new project </h1>
-    </>
+    <Container maxwidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align='center'>
+          Memories 
+        </Typography>
+        <img className={classes.image} src={Memories} alt="memories" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignitem='stretch' spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form /> 
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   )
 }
 
